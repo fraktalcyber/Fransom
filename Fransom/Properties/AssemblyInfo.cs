49272bcda52c031d532b45aeb3a139b2fdf05533
Tuomo Makkonen <tuomo@fraktal.fi>
2021-03-18 15:36:07 +0200

    Initial commit

---
+++ Fransom/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fransom")]
+[assembly: AssemblyDescription("Ransomware Emulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fraktal Ltd")]
+[assembly: AssemblyProduct("Fransom.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47483f01-e168-444d-a0cd-f0db7a287320")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
